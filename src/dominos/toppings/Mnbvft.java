8cd1d4bb6abb5edc858b17b47a2dcad348f0a809
Viknesh Soundararajan <csviknesh26@gmail.com>
2022-05-27 19:49:45 +0530

    qwerty

---
+++ src/dominos/toppings/Mnbvft.java
@@ -0,0 +1,30 @@
+package dominos.toppings;
+
+public class Mnbvft {
+
+	public static void main(String[] args) {
+		int var=5, sum=0;
+		
+		int num=12345;
+		
+		for (int i = 1; i <=num; i++) {
+		sum+= i;
+		}
+	System.out.println(sum);
+	while (num <=5) {
+		sum += num;
+	
+		
+		num++;
+	
+	}
+	
+	System.out.println(sum);
+	}
+	
+	
+	
+	
+	
+	
+}
